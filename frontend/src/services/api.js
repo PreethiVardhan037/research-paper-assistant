8ed6ae5929b1a9f95d38a55855d9cb0c97252346
PreethiVardhan037 <tpvardhan0307@gmail.com>
2026-07-31 10:00:13 +0530

    ready project for Azure Static Web Apps deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:7071/api",
+    baseURL: import.meta.env.MODE === "development"
+        ? "http://localhost:7071/api"
+        : "/api",
 });
 
 export default api;
